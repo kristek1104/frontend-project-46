f07a2939e6f3b4889351f290873ae774b37e3606
Kristina Martsinkevich <kristek1104@gmail.com>
2023-04-12 14:49:26 +0400

    fix with eslint

--- src/compareData.js
+++ src/compareData.js
@@ -1,59 +1,58 @@
 import _ from 'lodash';
 
 const compareData = (file1, file2) => {
-	const keys1 = _.keys(file1);
-	const keys2 = _.keys(file2);
-	const sortedKeys = _.sortBy(_.union(keys1, keys2));
+  const keys1 = _.keys(file1);
+  const keys2 = _.keys(file2);
+  const sortedKeys = _.sortBy(_.union(keys1, keys2));
 
-	const result = sortedKeys.map((key) => {
-		
-		//add
-		if (!_.has(file1, key)) {
-			return {
-				key,
-				value: file2[key],
-				type: 'added',
-			}
-		}
-		
-		//delete
-		if (!_.has(file2, key)) {
-			return {
-				key,
-				value: file1[key],
-				type: 'deleted',
-			}
-		}
+  const result = sortedKeys.map((key) => {
+    // add
+    if (!_.has(file1, key)) {
+      return {
+        key,
+        value: file2[key],
+        type: 'added',
+      };
+    }
 
-		//nested
-		if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
-			return {
-				key,
-				children: compareData(file1[key], file2[key]),
-				type: 'nested',
-			}
-		}
+    // delete
+    if (!_.has(file2, key)) {
+      return {
+        key,
+        value: file1[key],
+        type: 'deleted',
+      };
+    }
 
-		//change
-		if (_.has(file1, key) && _.has(file2, key)) {
-			if (file1[key] !== file2[key]) {
-				return {
-					key,
-					valueBefore: file1[key],
-					valueAfter: file2[key],
-					type: 'changed',
-				}
-			}
-			//unchange
-			return {
-				key,
-				value: file1[key],
-				type: 'unchanged',
-			}
-		}
-	});
+    // nested
+    if (_.isPlainObject(file1[key]) && _.isPlainObject(file2[key])) {
+      return {
+        key,
+        children: compareData(file1[key], file2[key]),
+        type: 'nested',
+      };
+    }
 
-	return result;
+    // change
+    if (_.has(file1, key) && _.has(file2, key)) {
+      if (file1[key] !== file2[key]) {
+        return {
+          key,
+          valueBefore: file1[key],
+          valueAfter: file2[key],
+          type: 'changed',
+        };
+      }
+
+      // unchange
+      return {
+        key,
+        value: file1[key],
+        type: 'unchanged',
+      };
+    }
+    return result;
+  });
 };
 
 export default compareData;
--- src/parsers.js
+++ src/parsers.js
@@ -1,15 +1,15 @@
 import { load } from 'js-yaml';
 
 const parse = (data, format) => {
-	if (format === 'json') {
-		return JSON.parse(data);
-	}
-	if (format === 'yml') {
-		return load(data);
-	}
-	if (format === 'yaml') {
-		return load(data);
-	}
-	throw new Error(`${format} is not supported`);
+  if (format === 'json') {
+    return JSON.parse(data);
+  }
+  if (format === 'yml') {
+    return load(data);
+  }
+  if (format === 'yaml') {
+    return load(data);
+  }
+  throw new Error(`${format} is not supported`);
 };
 export default parse;
