dcb43feef1ecafbebfcf39afbeeef4cd697dc64c
Kristina Martsinkevich <kristek1104@gmail.com>
2023-04-12 16:26:58 +0400

    fix after fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,18 +1,18 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander/esm.mjs';
-import gendiff from '../src/index.js';
 import process from 'node:process';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
 program
-	.version('0.1.0')
-	.description('Compares two configuration files and shows a difference.')
-	.arguments('<filepath1> <filepath2>')
-	.option('-f, --format <type>', 'output format', 'stylish')
-	.action((file1, file2, options) => {
-				console.log(gendiff(file1, file2, options.format))
-			});
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
+  .action((file1, file2, options) => {
+    console.log(gendiff(file1, file2, options.format));
+  });
 
 program.parse(process.argv);
--- src/formatters/format.js
+++ src/formatters/format.js
@@ -3,15 +3,15 @@ import formatPlain from './plain.js';
 import formatJson from './json.js';
 
 const format = (tree, formatName) => {
-		switch (formatName) {
-					case 'stylish':
-							return formatStylish(tree);
-						case 'plain':
-							return formatPlain(tree);
-						case 'json':
-							return formatJson(tree);
-						default:
-							throw new Error(`The ${formatName} format is not supported.\nSupported formats: stylish, plain, json.`);
-					}
+  switch (formatName) {
+    case 'stylish':
+      return formatStylish(tree);
+    case 'plain':
+      return formatPlain(tree);
+    case 'json':
+      return formatJson(tree);
+    default:
+      throw new Error(`The ${formatName} format is not supported.\nSupported formats: stylish, plain, json.`);
+  }
 };
 export default format;
