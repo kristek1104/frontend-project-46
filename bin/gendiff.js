f6e2b1c3c486f6abcad05a8aa3c6babe2f63fea0
Kristina Martsinkevich <kristek1104@gmail.com>
2023-04-12 15:19:16 +0400

    ff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -11,7 +11,7 @@ program
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format <type>', 'output format', 'stylish')
-  .action((file1, file2, options) => {
+  .action((filepath1, filepath2, options) => {
     console.log(gendiff(filepath1, filepath2, options.format));
   });
 
