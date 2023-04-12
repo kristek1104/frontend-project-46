ad35b254c3e188cf176909e3e2f170816788d335
Kristina Martsinkevich <kristek1104@gmail.com>
2023-04-12 14:23:07 +0400

    fix index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -4,7 +4,6 @@ import fs from 'fs';
 import { fileURLToPath } from 'url';
 import gendiff from '../src/index.js';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -16,20 +15,20 @@ const expectedResult2 = readFixture('fileOutput_plain.txt');
 const expectedResult3 = readFixture('fileOutput_json.txt');
 
 test('JSON', () => {
-	const file1 = getFixturePath('file1.json');
-	const file2 = getFixturePath('file2.json');
+  const file1 = getFixturePath('file1.json');
+  const file2 = getFixturePath('file2.json');
 
-	expect(gendiff(file1, file2)).toEqual(expectedResult1);
-	expect(gendiff(file1, file2, 'stylish')).toEqual(expectedResult1);
-	expect(gendiff(file1, file2, 'plain')).toEqual(expectedResult2);
-	expect(gendiff(file1, file2, 'json')).toEqual(expectedResult3);
+  expect(gendiff(file1, file2)).toEqual(expectedResult1);
+  expect(gendiff(file1, file2, 'stylish')).toEqual(expectedResult1);
+  expect(gendiff(file1, file2, 'plain')).toEqual(expectedResult2);
+  expect(gendiff(file1, file2, 'json')).toEqual(expectedResult3);
 });
 test('YML', () => {
-	const file1 = getFixturePath('file1.yml');
-	const file2 = getFixturePath('file2.yml');
+  const file1 = getFixturePath('file1.yml');
+  const file2 = getFixturePath('file2.yml');
 
-	expect(gendiff(file1, file2)).toEqual(expectedResult1);
-	expect(gendiff(file1, file2, 'stylish')).toEqual(expectedResult1);
-	expect(gendiff(file1, file2, 'plain')).toEqual(expectedResult2);
-	expect(gendiff(file1, file2, 'json')).toEqual(expectedResult3);
+  expect(gendiff(file1, file2)).toEqual(expectedResult1);
+  expect(gendiff(file1, file2, 'stylish')).toEqual(expectedResult1);
+  expect(gendiff(file1, file2, 'plain')).toEqual(expectedResult2);
+  expect(gendiff(file1, file2, 'json')).toEqual(expectedResult3);
 });
